d3afe25f5f53df7e695a56a67c352a4277f590f0
Flo Edelmann <florian-edelmann@online.de>
2022-11-23 15:10:13 +0100

    chore: simplify `useModules` logic

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,14 @@ export const registerModules = (modulesToRegister: Module[]) => {
 
 export const resetModules = () => modules.splice(0, modules.length)
 
-export const useModules = (moduleNames: string[]): Record<string, Module> => {
-  moduleNames.forEach(moduleName => {
-    if (!modules.some(registeredModule => registeredModule.name === moduleName)) {
+export const useModules = (moduleNames: string[]): Record<string, Module> => Object.fromEntries(
+  moduleNames.map(moduleName => {
+    const module = modules.find(registeredModule => registeredModule.name === moduleName)
+    
+    if (!module) {
       throw new Error(`vue-modulize: module "${moduleName}" is not registered`)
     }
-  })
-
-  return modules
-    .filter(registeredModule => moduleNames.some(moduleName => moduleName === registeredModule.name))
-    .reduce((previous, current) => ({ ...previous, [current.name]: current }), {})
-}
+    
+    return [moduleName, module]
+  }),
+)
